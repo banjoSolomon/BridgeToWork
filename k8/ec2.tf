b272644178d52ccfd2f2a0cb63cbaafba1da8a60
Banjo Solomon <151349000+banjoSolomon@users.noreply.github.com>
2025-09-05 00:45:51 +0100

    Update ec2.tf

--- k8/ec2.tf
+++ k8/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web_a" {
-  ami                    = "ami-08c40ec9ead489470" # Amazon Linux 2 (us-east-1)
+  ami                    = "ami-08c40ec9ead489470" 
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public_a.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
